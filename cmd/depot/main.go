f8f5e66f8630fc600980fe735f1cea3b2dd526e9
Alexander Metzner <alexander.metzner@gmail.com>
2021-04-03 18:30:33 +0200

    build: fix directory layout for depot cmd

--- cmd/depot/main.go
+++ cmd/depot/main.go
@@ -15,19 +15,25 @@ var (
 	readOnly    = flag.Bool("ro", false, "Generate a read-only repo")
 )
 
-// Usage: depot generate --table=messages ./test.go Message
+// Usage: depot generate-repo --table=messages ./test.go Message
 
 func main() {
 	flag.Parse()
 
-	if flag.NArg() != 2 {
+	if flag.NArg() != 3 {
 		fmt.Fprintf(os.Stderr, "%s: missing args\n", os.Args[0])
 		os.Exit(1)
 	}
 
+	if flag.Arg(0) != "generate-repo" {
+		fmt.Fprintf(os.Stderr, "%s: unknown command: %s\n", os.Args[0], flag.Arg(0))
+		os.Exit(1)
+
+	}
+
 	source, err := generate.GenerateRepository(generate.Options{
-		Filename:    flag.Arg(0),
-		EntityName:  flag.Arg(1),
+		Filename:    flag.Arg(1),
+		EntityName:  flag.Arg(2),
 		TableName:   *tableName,
 		RepoPackage: *repoPackage,
 		RepoName:    *repoName,
